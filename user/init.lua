5a2c9e43cbf024eae173a18e48f8a238320545e3
Eduardo Varela <54213198+EduardoV-dev@users.noreply.github.com>
2022-12-21 18:00:34 -0600

    Update init.lua

--- user/init.lua
+++ user/init.lua
@@ -3,9 +3,6 @@ local null_ls = require "null-ls"
 --              AstroNvim Configuration Table
 -- All configuration changes should go inside of the table below
 --
--- You can think of a Lua "table" as a dictionary like data structure the
--- normal format is "key = value". These also handle array like data structures
--- where a value with no key simply has an implicit numeric key
 local config = {
     -- set vim options here (vim.<first_key>.<second_key> = value)
     options = {
@@ -15,33 +12,28 @@ local config = {
         },
     },
 
-    -- Set colorscheme to use
-    colorscheme = "ayu-mirage",
+    colorscheme = "ayu-mirage", -- Set colorscheme to use
 
     -- Extend LSP configuration
     lsp = {
         formatting = {
-            timeout_ms = 2000, -- default format timeout
+            timeout_ms = 2000, -- format timeout
         },
     },
 
-    -- Mapping data with "desc" stored directly by vim.keymap.set().
-    --
-    -- Please use this mappings table to set keyboard mapping since this is the
-    -- lower level configuration and more robust one. (which-key will
-    -- automatically pick-up stored data by this setting.)
     mappings = {
         -- first key is the mode
         i = {
             ["kj"] = { "<esc>", desc = "Exit insert mode" },
-            -- setting a mapping to false will disable it
             ["jj"] = false,
+            -- setting a mapping to false will disable it
             ["jk"] = false,
             ["<C-h>"] = { "<C-W>", desc = "Simulate Ctrl + Backspace" }, -- <C-h> is the same as Ctrl+<BS>
             ["<C-Del>"] = { "<C-o>dw", desc = "Simulate Ctrl + Del" }
         },
         n = {
             ["q"] = false, -- Disables the record mode in vim
+            ["<leader>vs"] = { "<cmd>vsplit", desc = "Virtual split" }, -- Creates a virtual split for a selected file
         },
         t = {
             ["kj"] = { "<C-\\><C-n>", desc = "Exit terminal insert mode" }
@@ -51,20 +43,16 @@ local config = {
         }
     },
 
-
     -- Configure plugins
     plugins = {
         init = {
-            ["Shatur/neovim-ayu"] = {},
+            ["Shatur/neovim-ayu"] = {}, -- For theming with ayu.
+            ["tpope/vim-fugitive"] = {}, -- Git commands via nvim command mode.
+            ["tpope/vim-surround"] = {}, -- It is all about "surroundings": parentheses, brackets, quotes, XML tags, and more.
+            ["mg979/vim-visual-multi"] = {} -- Multi cursors in vim
         },
         -- All other entries override the require("<key>").setup({...}) call for default plugins
         ["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
-            -- config variable is the default configuration table for the setup function call
-            -- local null_ls = require "null-ls"
-
-            -- Check supported formatters and linters
-            -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-            -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
             config.sources = {
                 -- Set a formatter
                 null_ls.builtins.formatting.stylua,
